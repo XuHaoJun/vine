a828966b7d8523eda6cda514a94372824bd9494a
xuhaojuntw <xuhaojuntw@gmail.com>
2026-04-03 20:06:04 +0800

    refactor(layout): remove MainHeader from tabs layout for LINE-style header

--- apps/web/app/(app)/home/(tabs)/_layout.tsx
+++ apps/web/app/(app)/home/(tabs)/_layout.tsx
@@ -1,14 +1,5 @@
 import { Slot } from 'one'
-import { Spacer } from 'tamagui'
-
-import { MainHeader } from '~/features/app/MainHeader'
 
 export function TabsLayout() {
-  return (
-    <>
-      <MainHeader />
-      <Spacer height={50} />
-      <Slot />
-    </>
-  )
+  return <Slot />
 }
