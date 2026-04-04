1302ae70171ab864371933b34fc33b881a7aa9f1
xuhaojuntw <xuhaojuntw@gmail.com>
2026-04-04 09:40:21 +0800

    feat(chat): add DateSeparator component

---
+++ apps/web/src/features/chat/ui/DateSeparator.tsx
@@ -0,0 +1,25 @@
+import { SizableText, XStack } from 'tamagui'
+
+type Props = {
+  label: string
+}
+
+export function DateSeparator({ label }: Props) {
+  return (
+    <XStack justify="center" my="$2">
+      <XStack
+        bg="rgba(0,0,0,0.18)"
+        px="$3"
+        py={3}
+        rounded={99}
+      >
+        <SizableText
+          fontSize={11}
+          color="white"
+        >
+          {label}
+        </SizableText>
+      </XStack>
+    </XStack>
+  )
+}
