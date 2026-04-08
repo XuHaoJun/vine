590e9c19fc003b16384745d1afca7514de538b48
xuhaojuntw <xuhaojuntw@gmail.com>
2026-04-09 05:47:36 +0800

    feat(line-flex): add action utilities

---
+++ packages/line-flex/src/utils/action.ts
@@ -0,0 +1,12 @@
+import type { LFexAction } from '../types'
+
+export function handleAction(
+  action: LFexAction | undefined,
+  onAction: ((action: LFexAction) => void) | undefined
+): ((event: any) => void) | undefined {
+  if (!action || !onAction) return undefined
+  return (event: any) => {
+    event.preventDefault?.()
+    onAction(action)
+  }
+}
